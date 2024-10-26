fa8863f94a40c9c95ef56f9741d30d84361e6abd
joaovitornso <vitorjo8gbi@gmail.com>
2024-10-26 20:51:06 -0300

    feat(hotel): prepare controller and routes for hotels

---
+++ challenge_foco/app/Http/Controllers/HotelController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HotelController extends Controller
+{
+    public function hotels()
+    {
+        return "Hotels OK";
+    }
+}
