21937c5a9d8ccf53cb5187039a77f8c65a13a933
joaovitornso <vitorjo8gbi@gmail.com>
2024-10-27 14:04:27 -0300

    docs(reserve): adds route reserve status in Swagger Documentation

--- challenge_foco/app/Http/Controllers/ReserveController.php
+++ challenge_foco/app/Http/Controllers/ReserveController.php
@@ -4,9 +4,34 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+/**
+ * @OA\Schema(
+ *     schema="Reserve",
+ *     type="object",
+ *     description="Reserve model",
+ *     @OA\Property(property="id", type="integer", description="Reserve ID"),
+ *     @OA\Property(property="hotel_id", type="integer", description="ID of the associated hotel"),
+ *     @OA\Property(property="room_id", type="integer", description="ID of the associated room"),
+ *     @OA\Property(property="check_in", type="string", format="date", description="Check-in date in YYYY-MM-DD format"),
+ *     @OA\Property(property="check_out", type="string", format="date", description="Check-out date in YYYY-MM-DD format"),
+ *     @OA\Property(property="total", type="number", format="float", description="Total cost of the reservation"),
+ * )
+ */
 class ReserveController extends Controller
 {
 
+    /**
+     * @OA\Get(
+     *     path="/api/reserve/status",
+     *     tags={"Status"},
+     *     summary="Display API status",
+     *     description="Get the current status of the API",
+     *     @OA\Response(
+     *         response=200,
+     *         description="API is running"
+     *     )
+     * )
+     */
     public function status()
     {
         try{
