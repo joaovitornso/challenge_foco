65e96d53ccb0e4ad298736499ec74ff80943c141
joaovitornso <vitorjo8gbi@gmail.com>
2024-10-29 07:53:06 -0300

    add discounts method to show all discounts in database

--- challenge_foco/app/Http/Controllers/DiscountController.php
+++ challenge_foco/app/Http/Controllers/DiscountController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Discount;
 use Illuminate\Http\Request;
 
 class DiscountController extends Controller
 {
-    //
+
+
+    public function discounts()
+    {
+        try{
+            $discounts = Discount::paginate(10);
+            return response()->json(
+                [
+                    'status' => "OK",
+                    'message' => "success",
+                    'data' => $discounts
+                ],
+             200
+            );
+        } catch (\Exception $error) {
+            return response()->json(
+                [
+                    "error" => $error->getMessage(),
+                    'message' => "Error retrieving discounts data"
+                ],
+                404
+            );
+        }
+
+    }
 }
