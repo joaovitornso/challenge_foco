949413903554d2178167f6ee0afb1bc027446613
joaovitornso <vitorjo8gbi@gmail.com>
2024-10-29 08:10:20 -0300

    feat(Coupon): add createCoupon method to insert a new data in database

--- challenge_foco/app/Http/Controllers/CouponController.php
+++ challenge_foco/app/Http/Controllers/CouponController.php
@@ -64,4 +64,63 @@ class CouponController extends Controller
             );
         }
     }
+
+
+    public function createCoupon(Request $request)
+    {
+        try {
+
+            $request = $request->validate([
+                'code' => 'required|string|unique:coupon_codes,code',
+                'discount_type' => 'required|in:percent,fixed',
+                'value' => 'required|numeric',
+                'description' => 'nullable|string',
+                'start_date' => 'nullable|date',
+                'end_date' => 'nullable|date|after:start_date',
+                'max_uses' => 'nullable|integer|min:1',
+            ]);
+
+            // $coupon = new CouponCode();
+            // $coupon->code = $request->coupon;
+
+            // $coupon->code = $request->coupon;
+            // $coupon->discount_type = $request->discount_type;
+            // $coupon->value = $request->value;
+            // $coupon->description = $request->description;
+            // $coupon->start_date = $request->start_date;
+            // $coupon->end_date = $request->end_date;
+            // $coupon->max_uses = $request->max_uses;
+
+
+            $coupon = CouponCode::create($request);
+
+            // $coupon_saved = $coupon->save();
+
+            if($coupon){
+                return response()->json(
+                    [
+                        'status' => "OK",
+                        'message' => "Coupon created successfully",
+                        'data' => $coupon
+                    ],
+                    200);
+            } else {
+                return response()->json(
+                    [
+                        'status' => 'error',
+                        'message' => "Failed to save coupon"
+                    ],
+                    500);
+            }
+        } catch (\Exception $error) {
+            return response()->json(
+                [
+                    'error' => $error->getMessage(),
+                    'trace' => $error->getTraceAsString(),
+                    'message' => "An error occurred while saving the coupon"
+                ],
+                500
+            );
+        }
+    }
 }
