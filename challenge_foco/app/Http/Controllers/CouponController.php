c5f0d863fcbcff1d85e3aa84642ea6f621f63d8e
joaovitornso <vitorjo8gbi@gmail.com>
2024-10-29 07:58:31 -0300

    feat(Coupon): add coupon method to show only one coupon per id by POST method

--- challenge_foco/app/Http/Controllers/CouponController.php
+++ challenge_foco/app/Http/Controllers/CouponController.php
@@ -31,4 +31,37 @@ class CouponController extends Controller
             );
         }
     }
+
+    public function coupon(Request $request) #POST
+    {
+        try {
+            $coupon = CouponCode::find($request->id);
+            if ($coupon) {
+                return response()->json(
+                    [
+                        'status' => "OK",
+                        'message' => "success",
+                        'data' => $coupon
+                    ],
+                    200
+                );
+            } else {
+                return response()->json(
+                    [
+                        'status' => "error",
+                        'message' => "Coupon ID is required",
+                    ],
+                    400
+                );
+            }
+        } catch (\Exception $error) {
+            return response()->json(
+                [
+                    "error" => $error->getMessage(),
+                    "message" => "An unexpected error occurred while retrieving the coupon"
+                ],
+                500
+            );
+        }
+    }
 }
